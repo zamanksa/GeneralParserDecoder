db3fce2d0a89155d334391b9e13373e7b5e747ff
mzaman <mzaman@MZAMAN00TE11>
2024-09-07 09:59:52 +0300

    Add project files.

---
+++ GeneralParserDecoder/DataSources/cSerialPort.cs
@@ -0,0 +1,40 @@
+﻿using GeneralParserDecoder.DataSources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralParserDecoder.DataSources
+{
+    interface IDataSource
+    {
+        //int Rate; //Interface doesn not allow fields
+        //void event DataReceived;
+    }
+    internal class cDataReceivedArgs : IDataSource
+    {
+        Guid ID;
+        List<byte> Data;
+        DateTime ReceptionTime;
+    }
+    
+    internal class cSerialPort : IDataSource 
+    {
+    }
+
+    internal class cUDP : IDataSource
+    {
+
+    }
+
+    internal class cCAN : IDataSource 
+    {
+    }
+   
+    internal class cFile : IDataSource
+    {
+    }
+
+
+}
